8fa61c8bd55261a0dcf23e79a917d9daac72989b
Weber Gaia <webergaia@id.uff.br>
2022-06-17 01:30:34 -0300

    Interrogador 400Hz

--- 1. MATLAB - Dissertação LPG/6. Análise de dados/Interrogador_400Hz_LPG.m
+++ 1. MATLAB - Dissertação LPG/6. Análise de dados/Interrogador_400Hz_LPG.m
@@ -5,10 +5,92 @@
 %                     WEBER DE SOUZA GAIA FILHO                           %
 %                   TÉCNICAS DE INTERROGAÇÃO DE LPG                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% VARIÁVEIS DE LIMPEZA
+%% VARIÁVEIS DE LIMPEZA E DE ESCALA 
 clear all
 close all
 clc
-%% 
+xmin = 0;
+xmax = 2000;
+ymin = -50;
+ymax = 0;
+%% DISTORÇÃO 1%
 format long
-load('C:\Users\weber\Desktop\Resultados Experimentais\Fase 3\400Hz\Distorção 3.35%\1550nm')
+lambda = 1530;
+d = 1;
+[freq,P1,Lr,H1,H2,indice_H1,indice_H2] = Interrogador_400Hz(lambda,d);
+figure(1)
+plot(freq,10*log10(P1))                                                     % Plotagem gráfica
+xlim([xmin xmax])                                                           % Limitação do eixo X
+ylim([ymin ymax])                                                           % Limitação do eixo Y
+txt1 = ['\leftarrow H1: ',num2str(H1),' dB'];                               % Strings dinâmica para plotar o valor de H1
+txt2 = ['\leftarrow H2: ',num2str(H2),' dB'];                               % Strings dinâmica para plotar o valor de H2
+text(indice_H1,H1,txt1,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+text(indice_H2,H2,txt2,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+tit2 = ['Interrogador 400Hz LPG - FFT   ','(',num2str(lambda),'nm)   ','Distorção:',num2str(d)];                                  % Título do gráfico
+title(tit2) 
+sub = ['\color{red}', 'Lr:' num2str(Lr),'nm'];
+subtitle(sub,'FontWeight','bold')
+xlabel('Frequência (Hz)')
+ylabel('Amplitude (dB)')
+grid on
+grid minor
+%% DISTORÇÃO 2%
+lambda = 1530;
+d = 2;
+[freq,P1,Lr,H1,H2,indice_H1,indice_H2] = Interrogador_400Hz(lambda,d);
+figure(2)
+plot(freq,10*log10(P1))                                                     % Plotagem gráfica
+xlim([xmin xmax])                                                           % Limitação do eixo X
+ylim([ymin ymax])                                                           % Limitação do eixo Y
+txt1 = ['\leftarrow H1: ',num2str(H1),' dB'];                               % Strings dinâmica para plotar o valor de H1
+txt2 = ['\leftarrow H2: ',num2str(H2),' dB'];                               % Strings dinâmica para plotar o valor de H2
+text(indice_H1,H1,txt1,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+text(indice_H2,H2,txt2,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+tit2 = ['Interrogador 400Hz LPG - FFT   ','(',num2str(lambda),'nm)   ','Distorção:',num2str(d)];                                  % Título do gráfico
+title(tit2) 
+sub = ['\color{red}', 'Lr:' num2str(Lr),'nm'];
+subtitle(sub,'FontWeight','bold')
+xlabel('Frequência (Hz)')
+ylabel('Amplitude (dB)')
+grid on
+grid minor
+%% DISTORÇÃO 3%
+lambda = 1530;
+d = 3;
+[freq,P1,Lr,H1,H2,indice_H1,indice_H2] = Interrogador_400Hz(lambda,d);
+figure(3)
+plot(freq,10*log10(P1))                                                     % Plotagem gráfica
+xlim([xmin xmax])                                                           % Limitação do eixo X
+ylim([ymin ymax])                                                           % Limitação do eixo Y
+txt1 = ['\leftarrow H1: ',num2str(H1),' dB'];                               % Strings dinâmica para plotar o valor de H1
+txt2 = ['\leftarrow H2: ',num2str(H2),' dB'];                               % Strings dinâmica para plotar o valor de H2
+text(indice_H1,H1,txt1,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+text(indice_H2,H2,txt2,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+tit2 = ['Interrogador 400Hz LPG - FFT   ','(',num2str(lambda),'nm)   ','Distorção:',num2str(d)];                                  % Título do gráfico
+title(tit2) 
+sub = ['\color{red}', 'Lr:' num2str(Lr),'nm'];
+subtitle(sub,'FontWeight','bold')
+xlabel('Frequência (Hz)')
+ylabel('Amplitude (dB)')
+grid on
+grid minor
+%% DISTORÇÃO 3.35%
+lambda = 1530;
+d = 3.35;
+[freq,P1,Lr,H1,H2,indice_H1,indice_H2] = Interrogador_400Hz(lambda,d);
+figure(4)
+plot(freq,10*log10(P1))                                                     % Plotagem gráfica
+xlim([xmin xmax])                                                           % Limitação do eixo X
+ylim([ymin ymax])                                                           % Limitação do eixo Y
+txt1 = ['\leftarrow H1: ',num2str(H1),' dB'];                               % Strings dinâmica para plotar o valor de H1
+txt2 = ['\leftarrow H2: ',num2str(H2),' dB'];                               % Strings dinâmica para plotar o valor de H2
+text(indice_H1,H1,txt1,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+text(indice_H2,H2,txt2,'FontWeight','bold','FontSize',11)                   % Função para indicação no gráfico
+tit2 = ['Interrogador 400Hz LPG - FFT   ','(',num2str(lambda),'nm)   ','Distorção:',num2str(d)];                                  % Título do gráfico
+title(tit2) 
+sub = ['\color{red}', 'Lr:' num2str(Lr),'nm'];
+subtitle(sub,'FontWeight','bold')
+xlabel('Frequência (Hz)')
+ylabel('Amplitude (dB)')
+grid on
+grid minor
