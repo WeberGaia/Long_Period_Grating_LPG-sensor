f7a813564ceff35667ad234d9053b9a356f86abf
Weber Gaia <webergaia@id.uff.br>
2022-06-17 01:34:22 -0300

    Interrogador 1800Hz

---
+++ 1. MATLAB - Dissertação LPG/6. Análise de dados/Interrogador_1800Hz_LPG.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                   UNIVERSIDADE FEDERAL FLUMINENSE                       %         
+%   PROGRAMA DE PÓS-GRADUAÇÃO EM ENGENHARIA ELÉTRICA E TELECOMUNICAÇÕES   %
+%           ORIENTADOR: PROF. DR. ANDRÉS PABLO LOPEZ BARBERO              %
+%                     WEBER DE SOUZA GAIA FILHO                           %
+%                   TÉCNICAS DE INTERROGAÇÃO DE LPG                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% VARIÁVEIS DE LIMPEZA E DE ESCALA 
+clear all
+close all
+clc
