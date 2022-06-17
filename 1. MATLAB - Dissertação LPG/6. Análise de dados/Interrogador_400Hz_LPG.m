1f2fabbe4e39aee5d2d1a29e2bfa1f2f4ddc0e4c
Weber Gaia <webergaia@id.uff.br>
2022-06-16 22:59:12 -0300

    Análise de dados

---
+++ 1. MATLAB - Dissertação LPG/6. Análise de dados/Interrogador_400Hz_LPG.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                   UNIVERSIDADE FEDERAL FLUMINENSE                       %         
+%   PROGRAMA DE PÓS-GRADUAÇÃO EM ENGENHARIA ELÉTRICA E TELECOMUNICAÇÕES   %
+%           ORIENTADOR: PROF. DR. ANDRÉS PABLO LOPEZ BARBERO              %
+%                     WEBER DE SOUZA GAIA FILHO                           %
+%                   TÉCNICAS DE INTERROGAÇÃO DE LPG                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% VARIÁVEIS DE LIMPEZA
+clear all
+close all
+clc
+%% 
+format long
+load('C:\Users\weber\Desktop\Resultados Experimentais\Fase 3\400Hz\Distorção 3.35%\1550nm')
