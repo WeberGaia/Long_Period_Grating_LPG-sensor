d567b280a9656932ff0496b1289e4e39d4668471
Weber Gaia <webergaia@id.uff.br>
2022-06-21 13:39:11 -0300

    Gerador CSV c/ ruído

--- 1. MATLAB - Dissertação LPG/1. Gerador de Sinais AWG Picoscope/1.1 Gerador de sinal AWG/generator_CSV_ruido.m
+++ 1. MATLAB - Dissertação LPG/1. Gerador de Sinais AWG Picoscope/1.1 Gerador de sinal AWG/generator_CSV_ruido.m
@@ -2,9 +2,9 @@ function [banda,lbds,Lr] = generator_CSV_ruido(Lr)
 %UNTITLED Summary of this function goes here
 %% MODULADOR FBG
 Fs = 100000;                                                                % Frequência de Amostragem (Fs:100k)
-f = 1800;
+f = 1800;                                                                   % Frequência de modulação
 L = 11999;                                                                  % Comprimento do canal
-Ts = 1/(60*L);                                                             % Período de Amostragem
+Ts = 1/(60*L);                                                              % Período de Amostragem
 t = (0:L-1)*Ts;                                                             % Vetor tempo
 W0 = 2*pi*f*t;                                                              % Frequência Angular
 Lc = 1540;                                                                  % Comprimento de Onda da FBG (nm)
@@ -17,8 +17,8 @@ P0 = 0.31257;                                                               % Po
 a = 0.01;                                                                   % Largura de banda da banda de atenuação da LPG
 m = 0.84;                                                                   % Amplitude Normalizada
 lbds = 1500:0.01:1580;                                                      % Variação de lambdas (S)
-amp_noise = 0.001;
-ruido = randn(size(t))*amp_noise*P0;
+amp_noise = 0.001;                                                          % Porcentagem do ruído
+ruido = randn(size(t))*amp_noise*P0;                                        % Ruído
 banda = P0*(1-m*exp(-a*(Lr-lbds).^2));                                      % Banda de atenuação da LPG
 %% SINAL DEMODULADO DA LPG
 signal_dem = P0*(1-m*exp(-a*(Lr-Mod_FBG).^2))+ruido;
@@ -31,107 +31,107 @@ DC = signal_dem - level_DC;
 norm = (DC/max(abs(DC))*0.8);
 %% GRAVAÇÃO EM ARQUIVO CSV DOS VALORES CALCULADOS PARA O LAMBDA DE 1555nm
 if length(norm) == L && Lr == 1555
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1555nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1555nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1554
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1554nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1554nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif (length(norm) == L && Lr == 1553)
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1553nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1553nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1552
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1552nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1552nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1551
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1551nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1551nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1550
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1550nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1550nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1549
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1549nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1549nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1548
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1548nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1548nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1547
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1547nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1547nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1546
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1546nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1546nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1545
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1545nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1545nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1544
-     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1544nm.csv')
+     writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1544nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1543
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1543nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1543nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1542
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1542nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1542nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1541
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1541nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1541nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1540
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1540nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1540nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1539
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1539nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1539nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1538
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1538nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1538nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1537
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1537nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1537nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1536
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1536nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1536nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1535
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1535nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1535nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1534
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1534nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1534nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1533
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1533nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1533nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1532
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1532nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1532nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1531
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1531nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1531nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
 elseif length(norm) == L && Lr == 1530
-    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\2. CSV\1530nm.csv')
+    writematrix(norm','C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\6. Sinal demodulado LPG (teoria)\3. CSV ruido\1530nm.csv')
     X = ['Gravação concluída para ',num2str(Lr),'nm'];
     disp(X)
     disp('Fim da Gravação...')
